19929a21574707b3cc05942889c1a773fe45b91a
pjn141414 <jina071049@gmail.com>
2021-11-25 11:20:34 +0900

    Add: group api

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,19 @@ import { AuthModule } from './auth/auth.module';
 import { TokenModule } from './token/token.module';
 import { PostModule } from './post/post.module';
 import { CommentModule } from './comment/comment.module';
+import { GroupController } from './group/group.controller';
+import { GroupService } from './group/group.service';
+import { GroupModule } from './group/group.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), AuthModule, TokenModule, PostModule, CommentModule],
-  controllers: [],
+  imports: [TypeOrmModule.forRoot(config), AuthModule, TokenModule, PostModule, CommentModule, GroupModule],
+  controllers: [GroupController],
   providers: [
     {
       provide: APP_FILTER,
       useClass: CatchException,
     },
+    GroupService,
   ],
 })
 export class AppModule { }
