c57682f8509b2f0953a53a268985240bc752c5b9
pjn141414 <jina071049@gmail.com>
2021-11-25 14:59:32 +0900

    Add: vote api

--- src/vote/vote.module.ts
+++ src/vote/vote.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { VoteService } from './vote.service';
 import { VoteController } from './vote.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import PostVote from 'src/entities/PostVote';
+import CommentVote from 'src/entities/CommentVote';
+import User from 'src/entities/User';
+import Post from 'src/entities/Post';
+import Group from 'src/entities/Group';
+import PostLike from 'src/entities/PostLike';
+import Ban from 'src/entities/Ban';
+import Comment from 'src/entities/Comment';
+import { AuthService } from 'src/auth/auth.service';
+import { TokenService } from 'src/token/token.service';
+import { PostService } from 'src/post/post.service';
+import { GroupService } from 'src/group/group.service';
+import { CommentService } from 'src/comment/comment.service';
 
 @Module({
-  providers: [VoteService],
+  imports: [TypeOrmModule.forFeature([PostVote, CommentVote, User, Post, Group, PostLike, Comment, Ban])],
+  providers: [VoteService, AuthService, TokenService, PostService, GroupService, CommentService],
   controllers: [VoteController]
 })
-export class VoteModule {}
+export class VoteModule { }
--- src/vote/vote.service.ts
+++ src/vote/vote.service.ts
@@ -1,8 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthService } from 'src/auth/auth.service';
+import { CommentService } from 'src/comment/comment.service';
+import Ban from 'src/entities/Ban';
+import CommentVote from 'src/entities/CommentVote';
+import Post from 'src/entities/Post';
+import PostVote from 'src/entities/PostVote';
+import User from 'src/entities/User';
+import { GroupService } from 'src/group/group.service';
+import { PostService } from 'src/post/post.service';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class VoteService {
+  constructor(
+    @InjectRepository(PostVote)
+    private postVoteRepository: Repository<PostVote>,
+    @InjectRepository(CommentVote)
+    private commentReportRepository: Repository<CommentVote>,
+    private userService: AuthService,
+    private postService: PostService,
+    private groupService: GroupService,
+    private commentService: CommentService,
+    @InjectRepository(Ban)
+    private banRepository: Repository<Ban>
+  ) { }
 
+  public async addPostVote(idx: number, user: User, agree: boolean): Promise<void> {
+    const userData: User = await this.userService.getUserById(user.id);
+
+    const postData: Post = await this.postService.getPost(idx);
+
+    const reportList: PostVote[] = await this.postVoteRepository.find({
+      where: {
+        post: postData
+      }
+    });
+
+    if (reportList.length >= 10) {
+      const vote: PostVote = this.postVoteRepository.create({
+        user: userData,
+        post: postData,
+        agree: agree
+      });
+      await this.postVoteRepository.save(vote);
+    }
+  }
 }
+
+
 
 
