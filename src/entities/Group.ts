c793e03d5a859c4f1add8ca68ad2b0218ad5de5a
pjn141414 <jina071049@gmail.com>
2021-11-25 11:19:36 +0900

    Add: Group entity and relation tables

---
+++ src/entities/Group.ts
@@ -0,0 +1,68 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn, RelationId } from "typeorm";
+import Comment from "./Comment";
+import PostLike from "./PostLike";
+import User from "./User";
+
+@Entity('group')
+export default class Group {
+  @ApiProperty()
+  @PrimaryGeneratedColumn({ name: 'idx' })
+  idx!: number;
+
+  @ApiProperty()
+  @Column({ name: 'title' })
+  title!: string;
+
+  @ApiProperty()
+  @Column({ name: 'content' })
+  content!: string;
+
+  @ApiProperty()
+  @Column({ name: 'category' })
+  category!: string;
+
+  @ApiProperty()
+  @Column({ name: 'date' })
+  date!: Date;
+
+  @ApiProperty()
+  @Column({ name: 'place' })
+  place!: string;
+
+  @ApiProperty()
+  @Column({
+    nullable: true,
+  })
+  image?: string;
+
+  @ApiProperty()
+  @Column({
+    default: 0,
+    name: 'post_like'
+  })
+  postLike!: number;
+
+  @ApiProperty()
+  @CreateDateColumn({ name: 'create_at' })
+  createdAt: Date;
+
+  @ApiProperty()
+  @RelationId((group: Group) => group.user)
+  userId!: string;
+
+  @JoinColumn({ name: 'fk_user_id' })
+  @ManyToOne(() => User, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  user!: User;
+
+  @ApiProperty({ type: () => [Comment] })
+  @OneToMany(() => Comment, (comment) => comment.group)
+  comment!: Comment[];
+
+  @ApiProperty({ type: () => [PostLike] })
+  @OneToMany(() => PostLike, (postLike) => postLike.post)
+  postLikes!: PostLike[];
+}
